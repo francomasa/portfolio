bee31b19008270c5a15187ee53f42e56c87a0b97
Franco <francoezequielmassaccesi@gmail.com>
2025-08-03 18:18:42 -0300

    Modulo 29 Portfolio - intentando configurar plugins eslint e prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended', 'prettier'],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "ts/indent": "error"
+    }
+  },
+
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintConfigPrettier
+])
