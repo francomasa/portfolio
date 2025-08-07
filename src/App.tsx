1eccad8f5e7a543f280c9e2613d51dd957fb4a12
Franco <francoezequielmassaccesi@gmail.com>
2025-08-07 10:39:33 -0300

    revisar error com variaveis de thene

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import { ThemeProvider } from 'styled-components'
 
 import Sobre from './containers/Sobre'
 import Projetos from './containers/Projetos'
 import Sidebar from './containers/Sidebar'
 import EstiloGlobal, { Container } from './styles'
+import temaLight from './themes/light'
 
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaLight}>
       <EstiloGlobal />
         <Container>
         <Sidebar />
@@ -16,7 +18,7 @@ function App() {
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ src/themes/light.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'styled-components'
+
+const temaLight: DefaultTheme = {
+  corPrincipal: '#282A35',
+  corSecundaria: '#949494',
+  corDeFundo: '#fff',
+  corDeFondoBotao: '#282A35',
+  corDaBordaCard: '#c1c1c1',
+}
+
+export default temaLight
